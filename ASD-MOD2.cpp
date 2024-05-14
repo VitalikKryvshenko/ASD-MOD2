e1ce5d30700f8499b7c068555f81d541ba70f53c
Kryvshenko <23kryvshenko.v@nltu.lviv.ua>
2024-05-14 14:39:14 +0300

    Add project files.

---
+++ ASD-MOD2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+struct Point {
+    int x, y;
+};
+
+int orientation(Point p1, Point p2, Point p3) {
+    int val = (p2.y - p1.y) * (p3.x - p2.x) - (p2.x - p1.x) * (p3.y - p2.y);
+    if (val == 0) return 0;  
+    return (val > 0) ? 1 : 2; 
+}
+
+bool doIntersect(Point p1, Point q1, Point p2, Point q2) {
+    int o1 = orientation(p1, q1, p2);
+    int o2 = orientation(p1, q1, q2);
+    int o3 = orientation(p2, q2, p1);
+    int o4 = orientation(p2, q2, q1);
+
+    
+    if (o1 != o2 && o3 != o4)
+        return true;
+
+    if (o1 == 0 && p1.x >= p2.x && p1.x <= q2.x) return true;
+    if (o2 == 0 && q1.x >= p2.x && q1.x <= q2.x) return true;
+    if (o3 == 0 && p2.x >= p1.x && p2.x <= q1.x) return true;
+    if (o4 == 0 && q2.x >= p1.x && q2.x <= q1.x) return true;
+
+    return false; 
+}
+
+int main() {
+    setlocale(LC_ALL, "Ukrainian");
+    Point A = { 1, 1 }, B = { 4, 4 };
+    Point C = { 1, 4 }, D = { 4, 1 };
+
+    if (doIntersect(A, B, C, D))
+        cout << "Відрізки перетинаються\n";
+    else
+        cout << "Відрізки не перетинаються\n";
+
+    return 0;
+}
+
